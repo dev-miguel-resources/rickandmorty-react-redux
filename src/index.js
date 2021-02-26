4b494475d2b9871ceaf716e7a48cf118417a0b10
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-02-25 21:12:36 -0300

    configurando webpack & babel

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import reducer from './reducers';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import App from './routes/App';
 
 const initialState = {
